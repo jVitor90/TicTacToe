7e4d62d30842fcaef04a2dab280fbc2f9e495916
João Vitor Oliveira <joaooviitor2007@gmail.com>
2025-05-20 14:07:17 -0300

    Encerramento em caso de vítoria
    Implementação de sistema que encerra o jogo e bloqueia os botões caso haja um vencedor

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -89,57 +89,78 @@ namespace TicTacToe
             {
                 LblInfo.Text = $"O vencedor é {Btn11.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Horizontal 2
             else if (Btn21.Text == Btn22.Text && Btn22.Text == Btn23.Text && Btn21.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn21.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Horizontal 3
             else if (Btn31.Text == Btn32.Text && Btn32.Text == Btn33.Text && Btn31.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn31.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Vertical 1
             else if (Btn11.Text == Btn21.Text && Btn21.Text == Btn31.Text && Btn11.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn11.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Vertical 2
             else if (Btn12.Text == Btn22.Text && Btn22.Text == Btn32.Text && Btn12.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn12.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Vertical 3
             else if (Btn13.Text == Btn23.Text && Btn23.Text == Btn33.Text && Btn13.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn13.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Diagonal 1
             else if (Btn11.Text == Btn22.Text && Btn22.Text == Btn33.Text && Btn11.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn11.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Diagonal 2
             else if (Btn13.Text == Btn22.Text && Btn22.Text == Btn31.Text && Btn13.Text != "")
             {
                 LblInfo.Text = $"O vencedor é {Btn13.Text}";
                 LblInfo.ForeColor = Color.Green;
+                PararOJogo();
             }
             //Verificar se deu velha
             else if (!Btn11.Enabled && !Btn12.Enabled && !Btn13.Enabled && !Btn21.Enabled && !Btn22.Enabled &&
             Btn23.Enabled && !Btn31.Enabled && !Btn32.Enabled && !Btn33.Enabled)
             {
                 LblInfo.Text = "O jogo deu velha, Empate!";
-
             }
 
+            
+        }
+        public void PararOJogo()
+        {
+            //Desativar os botões, quando houver um vencedor para impedir que o jogo continue.
+            Btn11.Enabled = false;
+            Btn12.Enabled = false;
+            Btn13.Enabled = false;
+            Btn21.Enabled = false;
+            Btn22.Enabled = false;
+            Btn23.Enabled = false;
+            Btn31.Enabled = false;
+            Btn32.Enabled = false;
+            Btn33.Enabled = false;
         }
 
 
